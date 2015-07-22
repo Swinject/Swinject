1200e65ef6b716e8cfa91d6d4e1e29551a9db8b0
Yoichi Tagaya <yoichi@el-eleven.com>
2015-07-22 21:57:52 +0900

    Create a new project for Swinject.

---
+++ Swinject/Swinject.h
@@ -0,0 +1,19 @@
+//
+//  Swinject.h
+//  Swinject
+//
+//  Created by Yoichi Tagaya on 7/22/15.
+//  Copyright © 2015 DevSwinject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swinject.
+FOUNDATION_EXPORT double SwinjectVersionNumber;
+
+//! Project version string for Swinject.
+FOUNDATION_EXPORT const unsigned char SwinjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swinject/PublicHeader.h>
+
+
