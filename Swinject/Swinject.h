d07234f377fe431245a2adeb11e8b1b8c9f46347
Yoichi Tagaya <yoichi@el-eleven.com>
2015-07-23 00:13:41 +0900

    Add OSX and its test targets.

--- Swinject/Swinject.h
+++ Swinject/Swinject.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 DevSwinject. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Swinject.
 FOUNDATION_EXPORT double SwinjectVersionNumber;
