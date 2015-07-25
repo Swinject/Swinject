8f8e4f3a919f24ee517943d2a8b7a083bc33ad6d
Yoichi Tagaya <yoichi@el-eleven.com>
2015-07-25 15:37:50 +0900

    Group top level spec methods.

--- SwinjectTests/ContainerSpec.swift
+++ SwinjectTests/ContainerSpec.swift
@@ -12,47 +12,49 @@ import Nimble
 
 class ContainerSpec: QuickSpec {
     override func spec() {
-        it("resolves a registreed instance.") {
-            let container = Container()
-            container.register(BarType.self) { _ in Bar() }
-            
-            let bar = container.resolve(BarType.self)
-            expect(bar).notTo(beNil())
-        }
-        it("injects a resolved argument.") {
-            let container = Container()
-            container.register(BarType.self) { _ in Bar() }
-            container.register(FooType.self) { c in Foo(bar: c.resolve(BarType.self)!) }
+        describe("Basic resolution") {
+            it("resolves a registreed instance.") {
+                let container = Container()
+                container.register(BarType.self) { _ in Bar() }
                 
-            let foo = container.resolve(FooType.self) as? Foo
-            expect(foo?.bar).notTo(beNil())
-        }
-        it("resolves multiple initializers with arguments passed.") {
-            let container = Container()
-            container.register(BarType.self) { _ in Bar() }
-            container.register(BarType.self) { container, arg in Bar(arg1: arg) }
-            container.register(BarType.self) { container, arg1, arg2 in Bar(arg1: arg1, arg2: arg2) }
-            
-            let bar0 = container.resolve(BarType.self) as! Bar
-            let bar1 = container.resolve(BarType.self, arg1: "swift") as! Bar
-            let bar2 = container.resolve(BarType.self, arg1: "swinject", arg2: true) as! Bar
-            expect(bar0.arg1).to(beEmpty())
-            expect(bar1.arg1) == "swift"
-            expect(bar2.arg1) == "swinject"
-            expect(bar2.arg2) == true
-        }
-        it("resolves named services.") {
-            let container = Container()
-            container.register(BarType.self, name: "regA") { _ in Bar(arg1: "a") }
-            container.register(BarType.self, name: "regB") { _ in Bar(arg1: "b") }
-            container.register(BarType.self) { _ in Bar(arg1: "no name") }
-            
-            let a = container.resolve(BarType.self, name: "regA") as! Bar
-            let b = container.resolve(BarType.self, name: "regB") as! Bar
-            let noname = container.resolve(BarType.self) as! Bar
-            expect(a.arg1) == "a"
-            expect(b.arg1) == "b"
-            expect(noname.arg1) == "no name"
+                let bar = container.resolve(BarType.self)
+                expect(bar).notTo(beNil())
+            }
+            it("injects a resolved argument.") {
+                let container = Container()
+                container.register(BarType.self) { _ in Bar() }
+                container.register(FooType.self) { c in Foo(bar: c.resolve(BarType.self)!) }
+                    
+                let foo = container.resolve(FooType.self) as? Foo
+                expect(foo?.bar).notTo(beNil())
+            }
+            it("resolves multiple initializers with arguments passed.") {
+                let container = Container()
+                container.register(BarType.self) { _ in Bar() }
+                container.register(BarType.self) { container, arg in Bar(arg1: arg) }
+                container.register(BarType.self) { container, arg1, arg2 in Bar(arg1: arg1, arg2: arg2) }
+                
+                let bar0 = container.resolve(BarType.self) as! Bar
+                let bar1 = container.resolve(BarType.self, arg1: "swift") as! Bar
+                let bar2 = container.resolve(BarType.self, arg1: "swinject", arg2: true) as! Bar
+                expect(bar0.arg1).to(beEmpty())
+                expect(bar1.arg1) == "swift"
+                expect(bar2.arg1) == "swinject"
+                expect(bar2.arg2) == true
+            }
+            it("resolves named services.") {
+                let container = Container()
+                container.register(BarType.self, name: "regA") { _ in Bar(arg1: "a") }
+                container.register(BarType.self, name: "regB") { _ in Bar(arg1: "b") }
+                container.register(BarType.self) { _ in Bar(arg1: "no name") }
+                
+                let a = container.resolve(BarType.self, name: "regA") as! Bar
+                let b = container.resolve(BarType.self, name: "regB") as! Bar
+                let noname = container.resolve(BarType.self) as! Bar
+                expect(a.arg1) == "a"
+                expect(b.arg1) == "b"
+                expect(noname.arg1) == "no name"
+            }
         }
         describe("Container hierarchy") {
             it("resolves a service registered on the parent container.") {
