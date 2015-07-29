217ec17640067ff3de5d7a9556a24b7d5fc051cf
Yoichi Tagaya <yoichi@el-eleven.com>
2015-07-29 11:00:45 +0900

    Add tests for circular dependency of more than two objects.

--- SwinjectTests/Circularity.swift
+++ SwinjectTests/Circularity.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// MARK: Circular dependency of two objects
 internal protocol ParentType: AnyObject { }
 internal protocol ChildType: AnyObject { }
 
@@ -24,4 +25,49 @@ internal class Daughter: ChildType {
     init(parent: ParentType) {
         self.parent = parent
     }
+}
+
+// MARK: - Circular dependency of more than two objects
+internal protocol AType: AnyObject { }
+internal protocol BType: AnyObject { }
+internal protocol CType: AnyObject { }
+internal protocol DType: AnyObject { }
+
+internal class ADependingOnB: AType {
+    var b: BType?
+    
+    init() {
+    }
+    
+    init(b: BType) {
+        self.b = b
+    }
+}
+
+internal class BDependingOnC: BType {
+    var c: CType?
+    
+    init() {
+    }
+    
+    init(c: CType) {
+        self.c = c
+    }
+}
+
+internal class CDependingOnAD: CType {
+    weak var a: AType?
+    var d: DType?
+    
+    init() {
+    }
+    
+    init(d: DType) {
+        self.d = d
+    }
+}
+
+internal class DDependingOnBC: DType {
+    weak var b: BType?
+    weak var c: CType?
 }
--- SwinjectTests/ContainerSpec.swift
+++ SwinjectTests/ContainerSpec.swift
@@ -207,35 +207,96 @@ class ContainerSpec: QuickSpec {
             }
         }
         describe("Circualr dependency") {
-            it("resolves circular dependency by properties.") {
-                let container = Container()
-                container.register(ParentType.self) { _ in Mother() }
-                    .initCompleted { (c, i) in
-                        let mother = i as! Mother
-                        mother.child = c.resolve(ChildType.self)
-                    }
-                container.register(ChildType.self) { _ in Daughter() }
-                    .initCompleted { (c, i) in
-                        let daughter = i as! Daughter
-                        daughter.parent = c.resolve(ParentType.self)!
-                    }
-                
-                let daughter = container.resolve(ChildType.self) as! Daughter
-                let mother = daughter.parent as! Mother
-                expect(mother.child as? Daughter) === daughter
+            describe("of two objects") {
+                it("resolves circular dependency by properties.") {
+                    let container = Container()
+                    container.register(ParentType.self) { _ in Mother() }
+                        .initCompleted { (c, i) in
+                            let mother = i as! Mother
+                            mother.child = c.resolve(ChildType.self)
+                        }
+                    container.register(ChildType.self) { _ in Daughter() }
+                        .initCompleted { (c, i) in
+                            let daughter = i as! Daughter
+                            daughter.parent = c.resolve(ParentType.self)!
+                        }
+                    
+                    let daughter = container.resolve(ChildType.self) as! Daughter
+                    let mother = daughter.parent as! Mother
+                    expect(mother.child as? Daughter) === daughter
+                }
+                it("resolves circular dependency by an initializer and property.") {
+                    let container = Container()
+                    container.register(ParentType.self) { _ in Mother() }
+                        .initCompleted { (c, i) in
+                            let mother = i as! Mother
+                            mother.child = c.resolve(ChildType.self)
+                        }
+                    container.register(ChildType.self) { c in Daughter(parent: c.resolve(ParentType.self)!) }
+                    
+                    let daughter = container.resolve(ChildType.self) as! Daughter
+                    let mother = daughter.parent as! Mother
+                    expect(mother.child as? Daughter) === daughter
+                }
             }
-            it("resolves circular dependency by an initializer and property.") {
-                let container = Container()
-                container.register(ParentType.self) { c in Mother() }
-                    .initCompleted { (c, i) in
-                        let mother = i as! Mother
-                        mother.child = c.resolve(ChildType.self)
-                    }
-                container.register(ChildType.self) { c in Daughter(parent: c.resolve(ParentType.self)!) }
-                
-                let daughter = container.resolve(ChildType.self) as! Daughter
-                let mother = daughter.parent as! Mother
-                expect(mother.child as? Daughter) === daughter
+            describe("of more than two objects") {
+                it("resolves circular dependency by properties.") {
+                    let container = Container()
+                    container.register(AType.self) { _ in ADependingOnB() }
+                        .initCompleted {
+                            let a = $1 as! ADependingOnB
+                            a.b = $0.resolve(BType.self)
+                        }
+                    container.register(BType.self) { _ in BDependingOnC() }
+                        .initCompleted {
+                            let b = $1 as! BDependingOnC
+                            b.c = $0.resolve(CType.self)
+                        }
+                    container.register(CType.self) { _ in CDependingOnAD() }
+                        .initCompleted {
+                            let c = $1 as! CDependingOnAD
+                            c.a = $0.resolve(AType.self)
+                            c.d = $0.resolve(DType.self)
+                        }
+                    container.register(DType.self) { _ in DDependingOnBC() }
+                        .initCompleted {
+                            let d = $1 as! DDependingOnBC
+                            d.b = $0.resolve(BType.self)
+                            d.c = $0.resolve(CType.self)
+                        }
+                    
+                    let a = container.resolve(AType.self) as! ADependingOnB
+                    let b = a.b as! BDependingOnC
+                    let c = b.c as! CDependingOnAD
+                    let d = c.d as! DDependingOnBC
+                    expect(c.a as? ADependingOnB) === a
+                    expect(d.b as? BDependingOnC) === b
+                    expect(d.c as? CDependingOnAD) === c
+                }
+                it("resolves circular dependency by initializers and properties.") {
+                    let container = Container()
+                    container.register(AType.self) { cnt in ADependingOnB(b: cnt.resolve(BType.self)!) }
+                    container.register(BType.self) { cnt in BDependingOnC(c: cnt.resolve(CType.self)!) }
+                    container.register(CType.self) { cnt in CDependingOnAD(d: cnt.resolve(DType.self)!) }
+                        .initCompleted {
+                            let c = $1 as! CDependingOnAD
+                            c.a = $0.resolve(AType.self)
+                        }
+                    container.register(DType.self) { _ in DDependingOnBC() }
+                        .initCompleted {
+                            let d = $1 as! DDependingOnBC
+                            d.b = $0.resolve(BType.self)
+                            d.c = $0.resolve(CType.self)
+                        }
+                    
+                    let a = container.resolve(AType.self) as! ADependingOnB
+                    let b = a.b as! BDependingOnC
+                    let c = b.c as! CDependingOnAD
+                    let d = c.d as! DDependingOnBC
+                    expect(c.a as? ADependingOnB) === a
+                    expect(d.b as? BDependingOnC) === b
+                    expect(d.c as? CDependingOnAD) === c
+                }
             }
         }
     }
