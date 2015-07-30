78ebfe1aa24630acf46c4f4f00cb73f8cfc28b75
Yoichi Tagaya <yoichi@el-eleven.com>
2015-07-31 01:47:18 +0900

    Raises an exception when an infinite recursive call for circular dependency is detected.
    No error is thrown intentionally to avoid 'try' every time to resolve a service. Both to keep Swinject API (usage) simple and to inform the infinite call problem to programmers at development, an exception is intentionally used.

--- Swinject/ResolutionPool.swift
+++ Swinject/ResolutionPool.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 internal struct ResolutionPool {
+    private static let MaxDepth = 200
+    
     private var pool = [ServiceKey: Any]()
     private var depth: Int = 0
     
@@ -19,6 +21,17 @@ internal struct ResolutionPool {
     
     internal mutating func incrementDepth() {
         depth++
+        
+        if depth > ResolutionPool.MaxDepth {
+            // Raises an exception to tell the problem to programmers. (Usually just crashes the program.)
+            // No error is thrown intentionally to avoid 'try' every time to resolve a service.
+            let e = NSException(
+                name: "SwinjectInfiniteRecursiveCall",
+                reason: "Infinite recursive call for circular dependency has been detected. " +
+                        "To avoid the infinite call, 'initCompleted' handler should be used to inject circular dependency.",
+                userInfo: nil)
+            e.raise()
+        }
     }
     
     internal mutating func decrementDepth() {
