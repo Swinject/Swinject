51a02d504e9704626fc08ed1c4236d228d046319
Yoichi Tagaya <yoichi@el-eleven.com>
2015-08-03 13:54:24 +0900

    Add a named registration example to the playground.

--- Sample-iOS.playground/Contents.swift
+++ Sample-iOS.playground/Contents.swift
@@ -1,12 +1,16 @@
-//: # Swinject Sample for iOS
+/*:
+# Swinject Sample for iOS
+*/
 
-import UIKit
 import Swinject
 
-//: ## Basic Use
+/*:
+## Basic Use
+*/
 
 protocol AnimalType {
     var name: String? { get }
+    func sound() -> String
 }
 
 class Cat: AnimalType {
@@ -14,6 +18,10 @@ class Cat: AnimalType {
     
     init(name: String?) {
         self.name = name
+    }
+    
+    func sound() -> String {
+        return "Meow!"
     }
 }
 
@@ -30,16 +38,40 @@ class PetOwner: PersonType {
     
     func play() -> String {
         let name = pet.name ?? "someone"
-        return "I'm playing with \(name)."
+        return "I'm playing with \(name). \(pet.sound())"
     }
 }
 
+// Create a container and register service and component pairs.
 let container = Container()
 container.register(AnimalType.self) { _ in Cat(name: "Mimi") }
-container.register(PersonType.self) { r in
-    PetOwner(pet: r.resolve(AnimalType.self)!)
-}
+container.register(PersonType.self) { r in PetOwner(pet: r.resolve(AnimalType.self)!) }
 
+// The person is resolved to a PetOwner with a Cat.
 let person = container.resolve(PersonType.self)!
 print(person.play())
+
+/*:
+## Named Registration
+*/
+
+class Dog: AnimalType {
+    let name: String?
+    
+    init(name: String?) {
+        self.name = name
+    }
+    
+    func sound() -> String {
+        return "Bow wow!"
+    }
+}
+
+// Add more registrations to the container already containing the PetOwner with the Cat.
+container.register(AnimalType.self, name: "dog") { _ in Dog(name: "Hachi") }
+container.register(PersonType.self, name: "doggy") { r in PetOwner(pet: r.resolve(AnimalType.self, name: "dog")!) }
+
+// Resolve the service with the registration name to differentiate from the cat owner.
+let doggyPerson = container.resolve(PersonType.self, name:"doggy")!
+print(doggyPerson.play())
 
