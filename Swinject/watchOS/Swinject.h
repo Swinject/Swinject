905d2740a76bb70dec7d7dbdc997ba28580eb4b5
Yoichi Tagaya <yoichi@el-eleven.com>
2015-10-02 17:40:37 +0900

    Add watchOS target.

---
+++ Swinject/watchOS/Swinject.h
@@ -0,0 +1,19 @@
+//
+//  Swinject-watchOS.h
+//  Swinject-watchOS
+//
+//  Created by Yoichi Tagaya on 10/2/15.
+//  Copyright © 2015 Swinject Contributors. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Swinject-watchOS.
+FOUNDATION_EXPORT double Swinject_watchOSVersionNumber;
+
+//! Project version string for Swinject-watchOS.
+FOUNDATION_EXPORT const unsigned char Swinject_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swinject_watchOS/PublicHeader.h>
+
+
