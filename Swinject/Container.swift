ed97226e3e93141b312a78b14f172692519383bc
Yoichi Tagaya <yoichi@el-eleven.com>
2015-10-03 16:46:36 +0900

    Fix Issue #10 that dependency injection to view controllers in a storyboard does not work.
    The cause of the issue was that `entry.initCompleted as? (Resolvable, C) -> ()` in `Container` failed with Xcode 7.1 (beta). The cast with type `C` worked dynamically with Xcode 7.0, but not with Xcode 7.1. To avoid the problem, the type of initCompleted closure is always fixed to `(Resolvable, Controller) -> ()` by wrapping `(Resolvable, C) -> ()` type closure.
    
    The fix is the same as the commit 63a5e0000d9d501078f75672f9972f8c0505978f, which was applied for Xcode 6.4 with Swift 1.2.

--- Swinject/Container.swift
+++ Swinject/Container.swift
@@ -90,7 +90,11 @@ extension Container {
     public func registerForStoryboard<C: Controller>(controllerType: C.Type, name: String? = nil, initCompleted: (Resolvable, C) -> ()) {
         let key = ServiceKey(factoryType: controllerType, name: name)
         let entry = ServiceEntry(serviceType: controllerType)
-        entry.initCompleted = initCompleted
+        
+        // Xcode 7.1 workaround for Issue #10. This workaround is not necessary with Xcode 7.
+        let wrappingClosure: (Resolvable, Controller) -> () = { r, c in initCompleted(r, c as! C) }
+        entry.initCompleted = wrappingClosure
+        
         services[key] = entry
     }
     
@@ -101,7 +105,9 @@ extension Container {
         let key = ServiceKey(factoryType: controllerType, name: name)
         if let entry = getEntry(key) {
             resolutionPool[key] = controller as Any
-            if let completed = entry.initCompleted as? (Resolvable, C) -> () {
+            
+            // Xcode 7.1 workaround for Issue #10, casting initCompleted to (Resolvable, Controller) -> (), not to (Resolvable, C) -> ()
+            if let completed = entry.initCompleted as? ( (Resolvable, Controller) -> () ) {
                 completed(self, controller)
             }
         }
