aeaf15e30d3d0edc5fac5e005ca71cf9caeaf728
Yoichi Tagaya <yoichi@el-eleven.com>
2016-02-28 16:33:16 +0900

    Add ServiceKeyOptionType for extension/plugin for Swinject. The feature will be used by SwinjectStoryboard first.

--- Tests/ServiceKeySpec.swift
+++ Tests/ServiceKeySpec.swift
@@ -10,6 +10,24 @@ import Quick
 import Nimble
 @testable import Swinject
 
+// MARK: Option
+private struct Option: ServiceKeyOptionType {
+    let option: Int
+    
+    func isEqualTo(another: ServiceKeyOptionType) -> Bool {
+        guard let another = another as? Option else {
+            return false
+        }
+        
+        return self.option == another.option
+    }
+    
+    var hashValue: Int {
+        return option.hashValue
+    }
+}
+
+// MARK: ServiceKeySpec
 class ServiceKeySpec: QuickSpec {
     override func spec() {
         describe("Without name") {
@@ -74,6 +92,34 @@ class ServiceKeySpec: QuickSpec {
                 typealias FactoryType2Args = (Resolvable, String, Bool) -> AnimalType
                 let key3 = ServiceKey(factoryType: FactoryType2Args.self, name: "my factory")
                 let key4 = ServiceKey(factoryType: FactoryType2Args.self, name: "your factory")
+                expect(key3) != key4
+                expect(key3.hashValue) != key4.hashValue
+            }
+        }
+        describe("With option") {
+            it("equals with the same option.") {
+                typealias FactoryType0Args = Resolvable -> AnimalType
+                let key1 = ServiceKey(factoryType: FactoryType0Args.self, option: Option(option: 1))
+                let key2 = ServiceKey(factoryType: FactoryType0Args.self, option: Option(option: 1))
+                expect(key1) == key2
+                expect(key1.hashValue) == key2.hashValue
+                
+                typealias FactoryType2Args = (Resolvable, String, Bool) -> AnimalType
+                let key3 = ServiceKey(factoryType: FactoryType2Args.self, option: Option(option: 1))
+                let key4 = ServiceKey(factoryType: FactoryType2Args.self, option: Option(option: 1))
+                expect(key3) == key4
+                expect(key3.hashValue) == key4.hashValue
+            }
+            it("does not equal with different options.") {
+                typealias FactoryType0Args = Resolvable -> AnimalType
+                let key1 = ServiceKey(factoryType: FactoryType0Args.self, option: Option(option: 1))
+                let key2 = ServiceKey(factoryType: FactoryType0Args.self, option: Option(option: 2))
+                expect(key1) != key2
+                expect(key1.hashValue) != key2.hashValue
+                
+                typealias FactoryType2Args = (Resolvable, String, Bool) -> AnimalType
+                let key3 = ServiceKey(factoryType: FactoryType2Args.self, option: Option(option: 1))
+                let key4 = ServiceKey(factoryType: FactoryType2Args.self, option: Option(option: 2))
                 expect(key3) != key4
                 expect(key3.hashValue) != key4.hashValue
             }
