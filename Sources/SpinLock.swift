499c58ac3a5f81b3fc2208fddb3573a4de55f50d
Cameron Pulsford <cpulsfor@me.com>
2016-06-25 11:04:44 -0400

    Refactor the SpinLock class to use NSLock.
    http://engineering.postmates.com/Spinlocks-Considered-Harmful-On-iOS/

--- Sources/SpinLock.swift
+++ Sources/SpinLock.swift
@@ -9,11 +9,11 @@
 import Foundation
 
 internal final class SpinLock {
-    private var lock = OS_SPINLOCK_INIT
+    private let lock = NSLock()
     
     func sync<T>(@noescape action: () -> T) -> T {
-        OSSpinLockLock(&lock)
-        defer { OSSpinLockUnlock(&lock) }
+        lock.lock()
+        defer { lock.unlock() }
 
         return action()
     }
