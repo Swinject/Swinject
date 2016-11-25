45c2a34dc346e2bddea921fed97128ac9d4904a0
Yoichi Tagaya <yoichi@el-eleven.com>
2016-11-25 22:42:48 +0900

    Add an empty LinuxMain for unit tests.

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,13 @@
+//
+//  LinuxMain.swift
+//  Swinject
+//
+//  Created by Yoichi Tagaya on 11/25/16.
+//  Copyright © 2016 Swinject Contributors. All rights reserved.
+//
+
+import XCTest
+@testable import SwinjectTests
+
+XCTMain([
+])
