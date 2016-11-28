41b8002750e61d85c8a294a606c511ddb70a6e9e
Yoichi Tagaya <yoichi@el-eleven.com>
2016-11-28 12:20:01 +0900

    Add `@available` annotations for auto migration of .none and .hierarchy to .transient and .container scopes.

---
+++ Sources/UnavailableItems.swift
@@ -0,0 +1,16 @@
+//
+//  UnavailableItems.swift
+//  Swinject
+//
+//  Created by Yoichi Tagaya on 11/30/16.
+//  Copyright © 2016 Swinject Contributors. All rights reserved.
+//
+
+// MARK: For auto migration from Swinject v1 to v2.
+extension ObjectScope {
+    @available(*, unavailable, renamed: "transient")
+    public static let none = transient
+    
+    @available(*, unavailable, renamed: "container")
+    public static let hierarchy = container
+}
