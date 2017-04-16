ad195ca0f490ec28f781a4826d61b429d9eb9814
Yoichi Tagaya <yoichi@mercari.com>
2017-04-16 11:29:43 -0700

    Add a remark comment to the Container initializer taking a closure for performance.

--- Sources/Container.swift
+++ Sources/Container.swift
@@ -47,6 +47,7 @@ public final class Container {
     /// - Parameters:
     ///     - parent:             The optional parent `Container`.
     ///     - registeringClosure: The closure registering services to the new container instance.
+    /// - Remark: Compile time may be long if you pass a long closure to this initializer. Use `init()` or `init(parent:)` instead.
     public convenience init(parent: Container? = nil, registeringClosure: (Container) -> Void) {
         self.init(parent: parent)
         registeringClosure(self)
