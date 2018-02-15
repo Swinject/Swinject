550c33b3159eedcb88a5fc9c807820f9b1826614
Jakub Vaňo <me@jakubvano.com>
2018-02-15 10:46:33 +0000

    Fix type name in function docs

--- Sources/Container.swift
+++ Sources/Container.swift
@@ -63,13 +63,13 @@ public final class Container {
         services.removeAll()
     }
 
-    /// Discards instances for services registered in the given `ObjectsScopeType`.
+    /// Discards instances for services registered in the given `ObjectsScopeProtocol`.
     ///
     /// **Example usage:**
     ///     container.resetObjectScope(ObjectScope.container)
     ///
     /// - Parameters:
-    ///     - objectScope: All instances registered in given `ObjectsScopeType` will be discarded.
+    ///     - objectScope: All instances registered in given `ObjectsScopeProtocol` will be discarded.
     public func resetObjectScope(_ objectScope: ObjectScopeProtocol) {
         services.values
             .filter { $0.objectScope === objectScope }
