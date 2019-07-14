9fb96eb08791ba0cc68a9653fccf4a708275a3dc
Jakub Vaňo <me@jakubvano.com>
2019-07-14 17:57:26 +0200

    Integrate sourcery

---
+++ Tests/GeneratedCode.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2019 Swinject Contributors. All rights reserved.
+//
+
+// swiftlint:disable line_length
+// swiftlint:disable variable_name
+
+import Foundation
+#if os(iOS) || os(tvOS) || os(watchOS)
+    import UIKit
+#elseif os(OSX)
+    import AppKit
+#endif
