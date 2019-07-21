ba5c8e8419f7710c7b32c1fb967c7799c79dd9f7
Jakub Vaňo <me@jakubvano.com>
2019-07-21 21:50:58 +0200

    Implement resolver legacy api

--- Sources/2.0 API/Resolver+LegacyApi.swift
+++ Sources/2.0 API/Resolver+LegacyApi.swift
@@ -4,15 +4,6 @@
 
 // sourcery:inline:ResolverLegacyApi
 public extension Resolver {
-    /// Retrieves the instance with the specified service type.
-    ///
-    /// - Parameter serviceType: The service type to resolve.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no service is found.
-    func resolve<Service>(_ serviceType: Service.Type) -> Service? {
-        fatalError()
-    }
-
     /// Retrieves the instance with the specified service type and registration name.
     ///
     /// - Parameters:
@@ -20,20 +11,12 @@ public extension Resolver {
     ///   - name:        The registration name.
     ///
     /// - Returns: The resolved service type instance, or nil if no service with the name is found.
-    func resolve<Service>(_ serviceType: Service.Type, name: String?) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and 1 argument to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - argument:   1 Argument to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and 1 argument is found.
-    func resolve<Service, Arg1>(_ serviceType: Service.Type, argument: Arg1) -> Service? {
-        fatalError()
+    func resolve<Service>(_ serviceType: Service.Type, name: String? = nil) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: ()))
+        }
     }
 
     /// Retrieves the instance with the specified service type, 1 argument to the factory closure and registration name.
@@ -45,20 +28,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            1 argument and name is found.
-    func resolve<Service, Arg1>(_ serviceType: Service.Type, name: String?, argument: Arg1) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and list of 2 arguments to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - arguments:   List Of 2 Arguments to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and list of 2 arguments is found.
-    func resolve<Service, Arg1, Arg2>(_ serviceType: Service.Type, arguments arg1: Arg1, _ arg2: Arg2) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1>(_ serviceType: Service.Type, name: String? = nil, argument: Arg1) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: argument))
+        }
     }
 
     /// Retrieves the instance with the specified service type, list of 2 arguments to the factory closure and registration name.
@@ -70,20 +45,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            list of 2 arguments and name is found.
-    func resolve<Service, Arg1, Arg2>(_ serviceType: Service.Type, name: String?, arguments arg1: Arg1, _ arg2: Arg2) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and list of 3 arguments to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - arguments:   List Of 3 Arguments to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and list of 3 arguments is found.
-    func resolve<Service, Arg1, Arg2, Arg3>(_ serviceType: Service.Type, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1, Arg2>(_ serviceType: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: (arg1, arg2)))
+        }
     }
 
     /// Retrieves the instance with the specified service type, list of 3 arguments to the factory closure and registration name.
@@ -95,20 +62,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            list of 3 arguments and name is found.
-    func resolve<Service, Arg1, Arg2, Arg3>(_ serviceType: Service.Type, name: String?, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and list of 4 arguments to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - arguments:   List Of 4 Arguments to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and list of 4 arguments is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4>(_ serviceType: Service.Type, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1, Arg2, Arg3>(_ serviceType: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: (arg1, arg2, arg3)))
+        }
     }
 
     /// Retrieves the instance with the specified service type, list of 4 arguments to the factory closure and registration name.
@@ -120,20 +79,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            list of 4 arguments and name is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4>(_ serviceType: Service.Type, name: String?, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and list of 5 arguments to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - arguments:   List Of 5 Arguments to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and list of 5 arguments is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5>(_ serviceType: Service.Type, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1, Arg2, Arg3, Arg4>(_ serviceType: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: (arg1, arg2, arg3, arg4)))
+        }
     }
 
     /// Retrieves the instance with the specified service type, list of 5 arguments to the factory closure and registration name.
@@ -145,20 +96,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            list of 5 arguments and name is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5>(_ serviceType: Service.Type, name: String?, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and list of 6 arguments to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - arguments:   List Of 6 Arguments to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and list of 6 arguments is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6>(_ serviceType: Service.Type, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5>(_ serviceType: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: (arg1, arg2, arg3, arg4, arg5)))
+        }
     }
 
     /// Retrieves the instance with the specified service type, list of 6 arguments to the factory closure and registration name.
@@ -170,20 +113,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            list of 6 arguments and name is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6>(_ serviceType: Service.Type, name: String?, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and list of 7 arguments to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - arguments:   List Of 7 Arguments to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and list of 7 arguments is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(_ serviceType: Service.Type, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6>(_ serviceType: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: (arg1, arg2, arg3, arg4, arg5, arg6)))
+        }
     }
 
     /// Retrieves the instance with the specified service type, list of 7 arguments to the factory closure and registration name.
@@ -195,20 +130,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            list of 7 arguments and name is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(_ serviceType: Service.Type, name: String?, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and list of 8 arguments to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - arguments:   List Of 8 Arguments to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and list of 8 arguments is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8>(_ serviceType: Service.Type, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7, _ arg8: Arg8) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7>(_ serviceType: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: (arg1, arg2, arg3, arg4, arg5, arg6, arg7)))
+        }
     }
 
     /// Retrieves the instance with the specified service type, list of 8 arguments to the factory closure and registration name.
@@ -220,20 +147,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            list of 8 arguments and name is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8>(_ serviceType: Service.Type, name: String?, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7, _ arg8: Arg8) -> Service? {
-        fatalError()
-    }
-
-    /// Retrieves the instance with the specified service type and list of 9 arguments to the factory closure.
-    ///
-    /// - Parameters:
-    ///   - serviceType: The service type to resolve.
-    ///   - arguments:   List Of 9 Arguments to pass to the factory closure.
-    ///
-    /// - Returns: The resolved service type instance, or nil if no registration for the service type
-    ///            and list of 9 arguments is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9>(_ serviceType: Service.Type, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7, _ arg8: Arg8, _ arg9: Arg9) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8>(_ serviceType: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7, _ arg8: Arg8) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)))
+        }
     }
 
     /// Retrieves the instance with the specified service type, list of 9 arguments to the factory closure and registration name.
@@ -245,8 +164,12 @@ public extension Resolver {
     ///
     /// - Returns: The resolved service type instance, or nil if no registration for the service type,
     ///            list of 9 arguments and name is found.
-    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9>(_ serviceType: Service.Type, name: String?, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7, _ arg8: Arg8, _ arg9: Arg9) -> Service? {
-        fatalError()
+    func resolve<Service, Arg1, Arg2, Arg3, Arg4, Arg5, Arg6, Arg7, Arg8, Arg9>(_ serviceType: Service.Type, name: String? = nil, arguments arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3, _ arg4: Arg4, _ arg5: Arg5, _ arg6: Arg6, _ arg7: Arg7, _ arg8: Arg8, _ arg9: Arg9) -> Service? {
+        if let name = name {
+            return try? resolve(request(tag: name, arg: ()))
+        } else {
+            return try? resolve(request(tag: NoTag(), arg: (arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)))
+        }
     }
 
 }
