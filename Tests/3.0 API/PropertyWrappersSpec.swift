eb80388b398c888b94e4d8d8f454f5bda8838cb0
Jakub Vano <me@jakubvano.com>
2019-08-24 14:10:03 +0200

    Rewrite property wrappers spec

--- Tests/3.0 API/PropertyWrappersSpec.swift
+++ Tests/3.0 API/PropertyWrappersSpec.swift
@@ -17,7 +17,7 @@ class PropertyWrappersSpec: QuickSpec { override func spec() { #if swift(>=5.1)
         }
         it("can inject wrapped provider of a bound type") {
             let swinject = Swinject {
-                bbind(Int.self) & 42
+                register().constant(42)
             }
             let wrappedInt = try? swinject.instance() as Wrapped<Int>
             expect { wrappedInt?.wrappedValue } == 42
@@ -34,27 +34,27 @@ class PropertyWrappersSpec: QuickSpec { override func spec() { #if swift(>=5.1)
         }
         it("can inject lazy provider of bound type with arguments") {
             let swinject = Swinject {
-                bbind(Int.self) & factory { Int($1 as String)! }
+                register().factory { Int($1 as String)! }
             }
             let wrappedInt = try? swinject.instance(arg: "42") as Wrapped<Int>
             expect { wrappedInt?.wrappedValue } == 42
         }
         it("throws if provided type is bound with different tag") {
             let swinject = Swinject {
-                bbind(Int.self) & 42
+                register().constant(42)
             }
             expect { try swinject.instance(tagged: "tag") as Wrapped<Int> }.to(throwError())
         }
         it("can inject lazy provider of tagged bound type") {
             let swinject = Swinject {
-                bbind(Int.self, tagged: "tag") & 42
+                register().constant(42, tag: "tag")
             }
             let wrappedInt = try? swinject.instance(tagged: "tag") as Wrapped<Int>
             expect { wrappedInt?.wrappedValue } == 42
         }
         it("can inject nested property wrappers") {
             let swinject = Swinject {
-                bbind(Int.self) & 42
+                register().constant(42)
             }
             let doubleWrappedInt = try? swinject.instance() as Wrapped<Wrapped<Int>>
             expect { doubleWrappedInt?.wrappedValue.wrappedValue } == 42
@@ -64,8 +64,8 @@ class PropertyWrappersSpec: QuickSpec { override func spec() { #if swift(>=5.1)
                 @Wrapped var int: Int
             }
             let swinject = Swinject {
-                bbind(Int.self) & 42
-                bbind(IntHolder.self) & provider { try IntHolder(int: $0.instance()) }
+                register().constant(42)
+                register().factory { try IntHolder(int: $0.instance()) }
             }
             expect { try swinject.instance(of: IntHolder.self).int } == 42
         }
