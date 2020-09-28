46c3cf75628b62dea957ef85fe46913628b52d42
Marek Pridal <marek@marekpridal.eu>
2020-09-28 17:31:33 +0200

    Add debug symbols to xcframework

--- create-xcframework.sh
+++ create-xcframework.sh
@@ -4,7 +4,7 @@ xcodebuild archive \
 -project Swinject.xcodeproj \
 -scheme Swinject-iOS \
 -destination "generic/platform=iOS" \
--archivePath "archives/Swinject-iOS" \
+-archivePath "$PWD/archives/Swinject-iOS" \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -12,7 +12,7 @@ xcodebuild archive \
 -project Swinject.xcodeproj \
 -scheme Swinject-iOS \
 -destination "generic/platform=iOS Simulator" \
--archivePath "archives/Swinject-iOS-Simulator" \
+-archivePath "$PWD/archives/Swinject-iOS-Simulator" \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -20,7 +20,7 @@ xcodebuild archive \
 -project Swinject.xcodeproj \
 -scheme Swinject-watchOS \
 -destination "generic/platform=watchOS" \
--archivePath "archives/Swinject-watchOS" \
+-archivePath "$PWD/archives/Swinject-watchOS" \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -28,7 +28,7 @@ xcodebuild archive \
 -project Swinject.xcodeproj \
 -scheme Swinject-watchOS \
 -destination "generic/platform=watchOS Simulator" \
--archivePath "archives/Swinject-watchOS-Simulator" \
+-archivePath "$PWD/archives/Swinject-watchOS-Simulator" \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -36,7 +36,7 @@ xcodebuild archive \
 -project Swinject.xcodeproj \
 -scheme Swinject-tvOS \
 -destination "generic/platform=tvOS" \
--archivePath "archives/Swinject-tvOS" \
+-archivePath "$PWD/archives/Swinject-tvOS" \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -44,7 +44,7 @@ xcodebuild archive \
 -project Swinject.xcodeproj \
 -scheme Swinject-tvOS \
 -destination "generic/platform=tvOS Simulator" \
--archivePath "archives/Swinject-tvOS-Simulator" \
+-archivePath "$PWD/archives/Swinject-tvOS-Simulator" \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
@@ -52,16 +52,23 @@ xcodebuild archive \
 -project Swinject.xcodeproj \
 -scheme Swinject-macOS \
 -destination "generic/platform=macOS" \
--archivePath "archives/Swinject-macOS" \
+-archivePath "$PWD/archives/Swinject-macOS" \
 SKIP_INSTALL=NO \
 BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 xcodebuild -create-xcframework \
 -framework "Archives/Swinject-iOS.xcarchive/Products/Library/Frameworks/Swinject.framework" \
+-debug-symbols "$PWD/archives/Swinject-iOS.xcarchive/dSYMs/Swinject.framework.dSYM" \
 -framework "Archives/Swinject-iOS-Simulator.xcarchive/Products/Library/Frameworks/Swinject.framework" \
+-debug-symbols "$PWD/archives/Swinject-iOS-Simulator.xcarchive/dSYMs/Swinject.framework.dSYM" \
 -framework "Archives/Swinject-watchOS.xcarchive/Products/Library/Frameworks/Swinject.framework" \
+-debug-symbols "$PWD/archives/Swinject-watchOS.xcarchive/dSYMs/Swinject.framework.dSYM" \
 -framework "Archives/Swinject-watchOS-Simulator.xcarchive/Products/Library/Frameworks/Swinject.framework" \
+-debug-symbols "$PWD/archives/Swinject-watchOS-Simulator.xcarchive/dSYMs/Swinject.framework.dSYM" \
 -framework "Archives/Swinject-tvOS.xcarchive/Products/Library/Frameworks/Swinject.framework" \
+-debug-symbols "$PWD/archives/Swinject-tvOS.xcarchive/dSYMs/Swinject.framework.dSYM" \
 -framework "Archives/Swinject-tvOS-Simulator.xcarchive/Products/Library/Frameworks/Swinject.framework" \
+-debug-symbols "$PWD/archives/Swinject-tvOS-Simulator.xcarchive/dSYMs/Swinject.framework.dSYM" \
 -framework "Archives/Swinject-macOS.xcarchive/Products/Library/Frameworks/Swinject.framework" \
+-debug-symbols "$PWD/archives/Swinject-macOS.xcarchive/dSYMs/Swinject.framework.dSYM" \
 -output Swinject.xcframework
