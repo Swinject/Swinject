f86d11230923702e48f6009687892150f4b1dd79
Tomas Kohout <tomas.kohout@showmax.com>
2020-10-04 18:41:15 +0200

    Remove ssh from carthage

--- carthage_ci.sh
+++ carthage_ci.sh
@@ -18,4 +18,4 @@ echo 'EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_simulator__NATIVE_ARCH_64_BIT_x8
 echo 'EXCLUDED_ARCHS = $(inherited) $(EXCLUDED_ARCHS__EFFECTIVE_PLATFORM_SUFFIX_$(EFFECTIVE_PLATFORM_SUFFIX)__NATIVE_ARCH_64_BIT_$(NATIVE_ARCH_64_BIT)__XCODE_$(XCODE_VERSION_MAJOR))' >> $xcconfig
 
 export XCODE_XCCONFIG_FILE="$xcconfig"
-carthage bootstrap --use-ssh --new-resolver "$@"
+carthage bootstrap --new-resolver "$@"
