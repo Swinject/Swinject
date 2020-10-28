820191dbc6379a2b1595896ddcb6856674c112b7
Mark DiFranco <mpdifranco@icloud.com>
2020-10-28 15:31:39 -0400

    Update formatting of Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ import PackageDescription
 let package = Package(
     name: "Swinject",
     products: [
-        .library(name: "Swinject", targets: ["Swinject"]),
+        .library(name: "Swinject",
+                 targets: ["Swinject"]),
     ],
-    dependencies: [],
     targets: [
-        .target(name: "Swinject", dependencies: [], path: "Sources"),
+        .target(name: "Swinject",
+                path: "Sources"),
     ]
 )
