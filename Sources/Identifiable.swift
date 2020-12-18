9efc9b439b9dcb72a7ea4357a5a6d2ef48280ba4
Benjamin Lavialle <benjamin.lavialle@fabernovel.com>
2020-12-18 09:51:08 +0100

    Add type identification

---
+++ Sources/Identifiable.swift
@@ -0,0 +1,9 @@
+//
+//  Copyright © 2019 Swinject Contributors. All rights reserved.
+//
+
+import Foundation
+
+public protocol Identifiable {
+    associatedtype Identifier: TypeIdentifier
+}
---
+++ Sources/TypeIdentifier.swift
@@ -0,0 +1,9 @@
+//
+//  Copyright © 2019 Swinject Contributors. All rights reserved.
+//
+
+import Foundation
+
+public protocol TypeIdentifier {
+    var rawValue: String { get }
+}
