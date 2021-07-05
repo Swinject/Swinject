40c7f3f015ac906401e176fbf5f792be36d74a30
Lucas Maciel <lucasramos.m@gmail.com>
2021-07-04 22:57:26 -0300

    Removed temp script file

--- .github/scripts/test_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild "${{ matrix.ACTION }}" -project "${{ matrix.PROJECT }}" -scheme "${{ matrix.SCHEME }}" -sdk "${{ matrix.SDK }}" -destination "${{ matrix.DEST }}" -configuration Release ENABLE_TESTABILITY=YES | xcpretty
