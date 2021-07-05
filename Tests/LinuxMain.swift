8e5d4a662a3a29f0c77aaaf9908cb4fa287a6844
Lucas Maciel <lucasramos.m@gmail.com>
2021-07-05 15:28:58 -0300

    Replacing Quick Test with XCTest

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,19 +2,18 @@
 //  Copyright © 2019 Swinject Contributors. All rights reserved.
 //
 
-import Quick
 import XCTest
 
 @testable import SwinjectTests
 
-Quick.QCKMain([
-    AssemblerSpec.self,
-    ContainerSpec.self,
-    ContainerSpec_Arguments.self,
-    ContainerSpec_Circularity.self,
-    ContainerSpec_DebugHelper.self,
-    ContainerSpec_CustomScope.self,
-    SynchronizedResolverSpec.self,
-    ServiceKeySpec.self,
-    ServiceEntrySpec.self,
+XCTMain([
+    testCase(AssemblerTests.allTests),
+    testCase(ContainerTests.allTests),
+    testCase(ContainerTests.Arguments.allTests),
+    testCase(ContainerTests.Circularity.allTests),
+    testCase(ContainerTests.DebugHelper.allTests),
+    testCase(ContainerTests.CustomScope.allTests),
+    testCase(SynchronizedResolverTests.allTests),
+    testCase(erviceKeyTests.allTests),
+    testCase(ServiceEntryTests.allTests)
 ])
