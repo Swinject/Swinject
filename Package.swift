23deb1c7cf02240716ec0a3acccaf50bddf70ba0
Ion Ostafi <ion.ostafi@crunchyroll.com>
2024-01-15 22:29:49 +0200

    Update swift-tools and new platform visionos

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "Swinject",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v9),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .macOS(.v10_13),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v4),
+        .visionOS(.v1)
     ],
     products: [
         .library(name: "Swinject",
